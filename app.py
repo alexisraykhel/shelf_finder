bb08b2589dcaf8268471ed1dfcaa8810a91ef929
alexis <alexisizatt@gmail.com>
2021-07-24 13:12:39 -0500

    with fuzzy matching

---
+++ app.py
@@ -0,0 +1,64 @@
+import streamlit as st
+import pandas as pd
+from ast import literal_eval
+from fuzzywuzzy import process
+
+
+st.title('📚 To-Read Shelves 📚')
+
+
+def tidy_by_shelves(df):
+    """
+
+    :param df: the df with shelves
+    :return: new df with columns 'shelf', 'title', 'author'
+    """
+
+    new_rows = []
+    for x in df.iterrows():
+        row = x[1]
+        shelves = literal_eval(row.shelves)
+        for shelf in shelves:
+            new_rows.append({'shelf': shelf, 'Title': row.Title, 'Author': row.Author})
+
+    return pd.DataFrame(columns=['shelf', 'Title', 'Author'], data=new_rows)
+
+
+
+@st.cache
+def load_data():
+    return pd.read_csv(r"C:\Users\alexi\PycharmProjects\goodreads\goodreads_library_to_read_shelves.csv")
+
+data = load_data()
+
+data = tidy_by_shelves(data)
+
+st.subheader('Shelf Values')
+
+st.write(data.shelf.value_counts())
+
+st.write(data)
+
+st.subheader('Choose filters')
+
+shelf_options = list(set(data.shelf))
+shelf_filter = st.multiselect('Shelves', shelf_options)
+
+st.subheader(f'Only books in the following shelves: {", ".join(shelf_filter)}')
+
+st.write(data[data.shelf.isin(shelf_filter)])
+
+# todo: how about not super slow
+with_matches = []
+
+for x in data.iterrows():
+    row = x[1]
+    fuzzy_match = process.extractOne(row.shelf, shelf_options)
+
+    if fuzzy_match[1] >= 90:
+        with_matches.append({"shelf": fuzzy_match[0], "Title": row.Title, "Author": row.Author})
+    else:
+        continue
+
+
+st.write(pd.DataFrame(columns=["shelf", "Title", "Author"], data=with_matches))
